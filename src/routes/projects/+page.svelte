11b7fdfb31dedee3ea3e1a6adaceddc557ab03ab
Dana Hua <huadana1@gmail.com>
2025-03-10 17:57:38 -0400

    remove visible bar charts

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,9 +1,6 @@
 <script>
     import projects from "$lib/projects.json";
     import Project from "$lib/Project.svelte";
-
-    import Bar from "$lib/Bar.svelte";
-  import StackedBar from "$lib/StackedBar.svelte";
 </script>
 
 <svelte:head>
@@ -11,8 +8,6 @@
 </svelte:head>
 
 <h1>{projects.length} Projects</h1>
-<Bar data={projects}/>
-<StackedBar data={projects}/>
 <div class="projects">
     {#each projects as p}
         <Project data={p} />
